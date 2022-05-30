951a9faa4161d28fb3e087ea8f8232c0667fece4
Ming Wu <mingwucn@outlook.com>
2022-05-30 11:47:34 +0200

    try tf

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import './App.css';
 import BarChart from './Components/Chart/BarChart';
 import Canvas from './Components/Draw';
 import InputForm from './Components/InputForm';
-import LinePlot from "./Components/Chart/LinePlot"
+import TFModel from './Components/tf/TFModel';
 
-document.title="ECM predictor"
+document.title = "ECM predictor"
 
 function App() {
   return (
     <div className="App">
-        <InputForm />
-        {/* <BarChart /> */}
-        {/* <Canvas /> */}
-      </div>
+      <InputForm />
+      {/* <Canvas /> */}
+      <TFModel />
+    </div>
   );
 }
 
