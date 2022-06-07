a2afd6c6ca84a102c8b6e5946dca42b1a16d73ff
Ming Wu <mingwucn@outlook.com>
2022-06-07 20:22:10 +0200

    1.24

--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -1,10 +1,9 @@
-import { calculateNewValue } from '@testing-library/user-event/dist/utils';
-import { Scale } from 'chart.js';
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useTransition } from 'react';
 import './Slider.css';
 
 const Slider = (props) => {
-    const [show, setShow] = React.useState(0)
+    const [isPending, startTransition] = useTransition();
+    const [show, setShow] = useState(0)
     useEffect(() => {
         setShow(1);
         (() => setTimeout(() => {
@@ -19,10 +18,10 @@ const Slider = (props) => {
             <div className='field'>
                 <div className='left'>{`${props.minValue}>`}</div>
                 <div className='right' >{`<${props.maxValue}`}</div>
-                <div className="sliderValue" 
+                <div className="sliderValue"
                     style={{
                         color: "#e4e4e4",
-                        left: ((props.value - props.minValue) / (props.maxValue - props.minValue)) * 94 - 57 + "%",
+                        left: ((props.value - props.minValue) / (props.maxValue - props.minValue)) * 94 - 56 + "%",
                     }}
                 >
                     <span style={{
@@ -33,7 +32,11 @@ const Slider = (props) => {
                     className='sliderInput'
                     type="range" min={props.minValue} max={props.maxValue} step="0.5"
                     value={props.value}
-                    onChange={(e) => props.func(e.target.value)} />
+                    onChange={(e) => {
+                        startTransition(() =>
+                            props.func(e.target.value))
+                    }
+                    } />
             </div>
         </div>
     )
